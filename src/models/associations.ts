f2cf787ba23f4e1bfef1141e761c255a567b3d98
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2024-07-23 09:31:04 +0200

    add gift as model

--- src/models/associations.ts
+++ src/models/associations.ts
@@ -1,3 +1,5 @@
+import Gift from "./gift.model";
+import GiftDonor from "./giftdonor.model";
 import Room from "./room.model";
 import RoomUser from "./roomuser.model";
 import User from "./user.model";
@@ -11,4 +13,15 @@ User.belongsToMany(Room, {
   through: RoomUser,
   foreignKey: "UserId",
   as: "rooms",
+});
+
+User.belongsToMany(Gift, {
+  through: GiftDonor,
+  foreignKey: "userId",
+  as: "donatedGifts",
+});
+Gift.belongsToMany(User, {
+  through: GiftDonor,
+  foreignKey: "giftId",
+  as: "donors",
 });
