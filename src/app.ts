3512bf5f40621cc30a0c1df29ffed99d860e8793
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2024-09-05 13:19:29 +0200

    add localhost to auth

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,13 @@ import { apiKeyMiddleware } from "./middleware/apikey.middleware";
 
 export const app = express();
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // For legacy browser support
+};
+
 app.use(
-  cors(),
+  cors(corsOptions),
   apiKeyMiddleware,
   urlencoded({
     extended: true,
