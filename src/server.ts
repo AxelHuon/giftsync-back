26581e2419aa8773ec66348082254da18914de6d
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2024-09-05 18:55:38 +0200

    test

--- src/server.ts
+++ src/server.ts
@@ -8,15 +8,15 @@ import "dotenv/config";
 import "./models/associations";
 
 require("dotenv").config();
-
 const port = process.env.PORT;
-
 const cors = require("cors");
 
 const corsOptions = {
   origin: "http://localhost:3000",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
