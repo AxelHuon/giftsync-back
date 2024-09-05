6d36540e3d2cb5ad0b7b6d747925df99cf52c067
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2024-09-05 18:59:30 +0200

    test connection

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -2,6 +2,10 @@ import { Sequelize } from "sequelize";
 
 require("dotenv").config();
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL must be set");
+}
+
 const connection = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
