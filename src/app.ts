b374666c556b0efdd8e4d22f911e82d6b353eb9a
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2024-09-05 19:15:24 +0200

    add new config sequelize

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import express, { json, urlencoded } from "express";
 import { RegisterRoutes } from "../build/routes";
 import { apiKeyMiddleware } from "./middleware/apikey.middleware";
 
+const cors = require("cors");
+
 export const app = express();
 
+app.use(cors());
 app.use(apiKeyMiddleware);
 app.use(
   urlencoded({
